f80fb08139a40592de6f1709cff2734aaa8b579a
Sriram Alagarsamy <sriramsamy@gmail.com>
2016-11-13 15:39:45 +0530

    ProgrammingAssignment2
    Submitted by Sriram Alagarsamy

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+# makeCacheMatrix -> will receive a matrix as input and returns a 
+##list of functions set, get, setmatrix and get matrix
+#set -> This function will store the matrix received as input in the parent
+## environment and will blank out cache variable m
+#get -> will retrieve the input matrix
+#setmatrix->will store the inverse matrix in cache variable m
+#getmatrix->will retrieve the matrix in cache variable m
 
-## Write a short comment describing this function
+## makeCacheMatrix will be run once to create a list of functions. 
+#if a new matrix to be set call the function set() from the list of functions
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) 
+{
+  m<-NULL
+  set <-function (y)
+  {
+    x<<-y
+    m<<-NULL
+  }
+  get <- function() x
+  setmatrix <- function(nmat=matrix()) m <<- nmat
+  getmatrix <- function() m
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+#The returned list from makeCacheMatrix function should be passed as 
+#input to cachesolve
+
+cacheSolve <- function(x, ...) 
+{
         ## Return a matrix that is the inverse of 'x'
+  
+    mat<-x$getmatrix()
+    
+    if(!is.null(mat))
+    {
+      message("getting cached data")
+      return(mat)
+    }
+    data<-x$get()
+    print(data)
+    invMat<-solve(data)
+    output<-x$setmatrix(invMat)
+    return(output)
 }
+
